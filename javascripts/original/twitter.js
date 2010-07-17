4c1c806a842ab383c69f150c5fb9221336f36421
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2010-07-17 15:22:24 +0200

    Make links in tweets clickable.

--- javascripts/original/twitter.js
+++ javascripts/original/twitter.js
@@ -6,11 +6,11 @@ $(function() {
   if(statuses_count == null || status_text == null) {
     $.getJSON("http://twitter.com/users/show/svendahlstrand.json?callback=?", function(data) {
       $.cookie("statuses_count", data.statuses_count, { path: "/", expires: 0.003473 });
-      $.cookie("status_text", data.status.text, { path: "/" });
+      $.cookie("status_text", prepare_text(data.status.text), { path: "/" });
 
       box.text(data.statuses_count);
       box.fadeIn("slow", function() {
-        var paragraph = $('<div id="tweet"><p>' + data.status.text + "</p></div>").hide();
+        var paragraph = $('<div id="tweet"><p>' + prepare_text(data.status.text) + "</p></div>").hide();
         $("#aside").append(paragraph);
         paragraph.fadeIn("slow");
       });
@@ -21,3 +21,8 @@ $(function() {
     $("#aside").append('<div id="tweet"><p>' + status_text + "</p></div>");
   }
 });
+
+function prepare_text(text) {
+  var expression = /http:\/\/((?:www.)?[a-z0-9\/]+\.[a-z0-9\/]+)\s?/;
+  return text.replace(expression, "<a href=\"http://$1\">$&</a>");
+}

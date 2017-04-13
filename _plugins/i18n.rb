dabbd1f8acf2d20786d95df631ceca8065d9a109
Sven Dahlstrand <sven@dahlstrand.net>
2017-04-13 09:44:38 +0200

    Add simple translation plugin.

---
+++ _plugins/i18n.rb
@@ -0,0 +1,23 @@
+require('json')
+
+module Jekyll
+  # Simple helper for translations. Example usage:
+  #
+  # {% t my_key %}
+  class TranslateTag < Liquid::Tag
+    def initialize(tag_name, key, tokens)
+      super
+
+      @key = key.strip
+    end
+
+    def render(context)
+      i18n = context['site.data.i18n']
+      language = context['page.language']
+
+      i18n[language][@key]
+    end
+  end
+end
+
+Liquid::Template.register_tag('t', Jekyll::TranslateTag)
